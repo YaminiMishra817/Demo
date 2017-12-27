ea8095d44c5092e8cb4346b5700cd877826e1960
YaminiMishra817 <34883212+YaminiMishra817@users.noreply.github.com>
2017-12-27 15:54:46 +0530

    Initial commit
    des

---
+++ WebApplication2/WebApplication2/Models/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Demo
+    {
+
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public int Pincode { get; set; }
+
+    }
+}
